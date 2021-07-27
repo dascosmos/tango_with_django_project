d48ed6f2509192abcb9cd00c7270e6b4cdcc0cf9
David Salazar <david.salazar033@outlook.com>
2021-07-27 19:21:41 +0100

    Add static files, add context to html pages and create path for dinamic files according to Chapter 4

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rango/', include('rango.urls')),
     path('', views.index, name='index'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
