2b9916af4138eeb60ab0d3c2d24e4e7344ed8380
David Salazar <david.salazar033@outlook.com>
2021-07-28 17:38:16 +0100

    Add about page and content to media directory

--- rango/views.py
+++ rango/views.py
@@ -8,3 +8,9 @@ def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 
     return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    context = {'boldmessage': 'David Salazar'}
+
+    return render(request, 'rango/about.html', context=context)
