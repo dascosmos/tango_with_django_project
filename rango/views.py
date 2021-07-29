70e01a0c29594fe31afb525ff0e0232f8d22cdb6
David Salazar <david.salazar033@outlook.com>
2021-07-29 02:21:14 +0100

    add workspace files to gitignore, create category vew and template and modify index.html according to chapter 6

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 
 # Create your views here.
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    categpry_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = categpry_list
 
     return render(request, 'rango/index.html', context=context_dict)
 
@@ -14,3 +18,20 @@ def about(request):
     context = {'boldmessage': 'David Salazar'}
 
     return render(request, 'rango/about.html', context=context)
+
+def show_category(request, category_name_slug):
+
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category']= category
+
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
